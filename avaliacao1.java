3cf09bafdc8e27387f0dc594895f75b80bca712a
Rodrigo Maia <roderictus@gmail.com>
2014-06-22 11:07:05 -0300

    Avaliação 1
    Teste da Questão 4

---
+++ avaliacao1.java
@@ -0,0 +1,14 @@
+public class avaliacao1 {
+
+	public static void main (String [] args){
+	
+	int i = 9;
+	while (i <= 10) { 
+	System.out.println(i + 1); 
+	} 
+	System.out.print(i);
+	/* como o i nunca deixa de ser 9, fica imprimindo 10 infinitamente, 
+	pois "i + 1" não acrescenta ao "i" pois não tem operação matemática*/
+	}
+
+}
